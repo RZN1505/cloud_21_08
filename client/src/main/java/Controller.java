f86cdc8f85eb0b90c5ba709f9bf7614a6f26db98
ruarez <zlata.reznikova@raiffeisen.ru>
2020-08-28 15:10:56 +0300

    lesson2

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -19,8 +19,10 @@ import java.util.concurrent.ExecutionException;
 
 public class Controller implements Initializable {
 
-    public ListView<String> listView;
+    public ListView<String> listViewClient;
+    public ListView<String> listViewServer;
     public TextField text;
+    public TextField textStatus;
     public Button send;
     private Socket socket;
     private static DataInputStream is;
@@ -30,10 +32,13 @@ public class Controller implements Initializable {
     InputStream in;
     OutputStream out;
     private String clientPath = "client/ClientStorage";
-    private String [] commands = {"/upload&"};
+    private String[] commands = {"/upload&", "/download&"};
     private HashSet<String> sendFiles = new HashSet<>();
+    private HashSet<String> recieveFiles = new HashSet<>();
     private File dir;
-    String oldText ="";
+    String oldText = "";
+    String oldTextOneClick = "";
+
 
     public static void stop() {
         try {
@@ -43,50 +48,84 @@ public class Controller implements Initializable {
             e.printStackTrace();
         }
     }
-    synchronized public void sendFile(File file) throws IOException {
-        System.out.println("transferFile2=" + file.getName() + " " + (int) file.length());
 
-        String message = text.getText();
-
+    public void recieveFile(String command, String fileName) throws IOException {
+        System.out.println("recievetName() " + fileName);
         try {
-
-            byte[] mybytearray = new byte[(int) file.length()];
-
-            fis = new FileInputStream(file);
-
-            is = new DataInputStream(fis);
-
-            byte[] buffer = new byte[5];
-            boolean var5 = false;
-
-            int count;
-            while((count = is.read(buffer)) != -1) {
-                os.write(buffer, 0, count);
-                System.out.println("readClient " + count + " bytes");
+            os.writeUTF(command);
+            os.writeUTF(fileName);
+            String response = is.readUTF();
+            System.out.println("OKres" + response);
+            if (response.equals("OK")) {
+                System.out.println("OK");
+                long fileLength = is.readLong();
+            System.out.println("fileLength" + fileLength);
+                File file = new File("client/ClientStorage/" + fileName);
+                file.createNewFile();
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    byte[] buffer = new byte[256];
+                    if (fileLength < 256) {
+                        fileLength += 256;
+                    }
+                    int read = 0;
+                    for (int i = 0; i < fileLength / 256; i++) {
+                        read = is.read(buffer);
+                        fos.write(buffer, 0, read);
+                    }
+                    /*if (file.length() == fileLength) {
+                        System.out.println("fileLength3=" );*/
+                        textStatus.setText("File downloaded!");
+                    //}
+                }
+            } else {
+                textStatus.setText("file not found!");
             }
-            os.flush();
-
-            closeConnection();
-            System.out.println("Done");
-        } catch (IOException ex) {
-            System.out.println("Errr");
         } catch (Exception e) {
-            System.err.println(e + " file to server send");;
+            e.printStackTrace();
         }
     }
+
+     public void sendFile(File file, String command, String fileName) throws IOException {
+        System.out.println("transferFile2=" + file.getName() + " " + (int) file.length());
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            os.writeUTF(command);
+            os.writeUTF(fileName);
+            os.writeLong(file.length());
+            byte[] buffer = new byte[256];
+            int read = 0;
+            while ((read = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+            String response = is.readUTF();
+            if (response.equals("OK")) {
+                textStatus.setText("File uploaded!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void sendMessage(ActionEvent actionEvent) {
         String message = text.getText();
-
+        String[] tokens = message.split("&");
+        String command = tokens[0];
 
         try {
             os.writeUTF(message);
             os.flush();
-
-            for (File file : dir.listFiles()) {
-                if (sendFiles.contains(file.getName())) {
-                    System.out.println("transferFileList" + file.getName() + " " + (int) file.length());
-                    sendFile(file);
+            if (command.equals("/upload")) {
+                for (File file : dir.listFiles()) {
+                    if (sendFiles.contains(file.getName())) {
+                        System.out.println("transferFileList" + file.getName() + " " + (int) file.length());
+                        sendFile(file, command, file.getName());
+                    }
                 }
+            } else if (command.equals("/download")) {
+                //for (String fileName : recieveFiles) {
+                    recieveFile(command, tokens[1]);
+                //}
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -95,44 +134,42 @@ public class Controller implements Initializable {
     }
 
     public void connect(/*String host, int port*/) throws Exception {
-        socket = new Socket("localhost", 8189);;
-
+        socket = new Socket("localhost", 8189);
         in = this.socket.getInputStream();
         out = this.socket.getOutputStream();
         is = new DataInputStream(in);
         os = new DataOutputStream(out);
     }
 
-    public void closeConnection() throws Exception {
-        in.close();
-        out.close();
-        os.close();
-        is.close();
-        socket.close();
-    }
+
     public void initialize(URL location, ResourceBundle resources) {
         dir = new File(clientPath);
 
         for (File file : dir.listFiles()) {
-            listView.getItems().add(file.getName()/* + "|" + file.length() + " bytes"*/);
+            listViewClient.getItems().add(file.getName()/* + "|" + file.length() + " bytes"*/);
         }
 
         text.setOnAction(this::sendMessage);
 
-        listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        listViewClient.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent click) {
-
+                String currentItemSelected = (String) listViewClient.getSelectionModel()
+                        .getSelectedItem();
+                String str = currentItemSelected;
                 if (click.getClickCount() == 2) {
-                    String currentItemSelected = (String) listView.getSelectionModel()
-                            .getSelectedItem();
-                    String str = currentItemSelected/*.split("|")*/;
-                    String oldTextHelper = oldText != "" ? oldText : commands[0];
-                    oldText = oldTextHelper  + ", " + str;
+                    String oldTextHelper = oldText != "" ? oldText + ", " : commands[0];
+                    oldText = oldTextHelper + str;
                     text.setText(oldText);
                     System.out.println("currentItemSelected" + text.getText());
                     sendFiles.add(str);
+                } else if (click.getClickCount() == 1) {
+                    String oldTextHelperOne = oldTextOneClick != "" ? oldTextOneClick + ", " : commands[1];
+                    oldTextOneClick = oldTextHelperOne + str;
+                    text.setText(oldTextOneClick);
+                    System.out.println("currentItemSelectedOne" + text.getText());
+                    recieveFiles.add(str);
                 }
             }
         });
@@ -154,7 +191,7 @@ public class Controller implements Initializable {
 //                    }
 //                }
 //            };
-            new Thread(() -> {
+            /*new Thread(() -> {
                 while (true) {
                     try {
                         String message = is.readUTF();
@@ -166,7 +203,7 @@ public class Controller implements Initializable {
                         e.printStackTrace();
                     }
                 }
-            }).start();
+            }).start();*/
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (Exception e) {
--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -30,22 +30,55 @@ public class ClientHandler implements Runnable {
         File dir = ClientHandler.createDirectory("client" + name + "Dir");
         os.flush();
     }*/
-   synchronized public void recieveFiles(String message) throws IOException {
+   /*synchronized*/ public void recieveFiles(String message) throws IOException {
+       String fileName = is.readUTF();
+       long fileLength = is.readLong();
        System.out.println("recieveFiles" + message);
-        File dir = createDirectory("client" + name + "Dir");
-        //String [] strFiles = message.split("&")[1].split(",");
-        OutputStream osf = new FileOutputStream(dir + "/" + "fileName");
-        byte[] buffer = new byte[1024];
-
-        int count = 0;
-        while((count = is.read(buffer)) != -1) {
-            osf.write(buffer, 0, count);
-            System.out.println("read " + count + " bytes");
+       File dir = createDirectory("client" + name + "Dir");
+       //String [] strFiles = message.split("&")[1].split(",");
+      File file = new File(dir + "/" + fileName);
+      file.createNewFile();
+       try (FileOutputStream fos = new FileOutputStream(file)) {
+           byte[] buffer = new byte[256];
+           if (fileLength < 256) {
+               fileLength += 256;
+           }
+           int read = 0;
+           for (int i = 0; i < fileLength / 256; i++) {
+               read = is.read(buffer);
+               fos.write(buffer, 0, read);
+           }
+           os.writeUTF("OK");
+           ;
+           is.close();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+   }
+    /*synchronized*/ public void sendFiles(String message) throws IOException {
+        String fileName = is.readUTF();
+        System.out.println("sendFiles" + fileName);
+        File dir = createDirectory("server/ServerStorage");
+        File file = new File(dir + "/" + fileName);
+        if (file.exists()) {
+            os.writeUTF("OK");
+            System.out.println("sendFilesOk" + file.getAbsolutePath());
+        } else {
+            //file.createNewFile();
+            os.writeUTF("WRONG");
+            System.out.println("sendFilesNOTOk");
         }
-        osf.close();
-        is.close();
+        try (FileInputStream fis = new FileInputStream(file)) {
+            os.writeLong(file.length());
+            byte[] buffer = new byte[256];
+            int read = 0;
+            while ((read = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+        }
     }
-    synchronized public File createDirectory(String directoryPath) throws IOException {
+   /*synchronized*/ public File createDirectory(String directoryPath) throws IOException {
         File dir = new File(directoryPath);
         if (dir.exists()) {
             return dir;
@@ -55,27 +88,29 @@ public class ClientHandler implements Runnable {
         }
         throw new IOException("Failed to create directory '" + dir.getAbsolutePath() + "' for an unknown reason.");
     }
-    public void closeConnection() throws Exception {
-        os.close();
-        is.close();
-        socket.close();
-        System.out.println("client " + name + " disconnected");
-    }
+
     public void run() {
         while (true) {
             try {
                 String message = is.readUTF();
                 System.out.println("message from " + name + ": " + message);
-                server.broadCastMessage(message);
+                //server.broadCastMessage(message);
                 if (message.equals("quit")) {
-                    closeConnection();
-                    server.kick(this);
+                    os.writeUTF("disconnected");
+                    Thread.sleep(1000);
+                    os.close();
+                    is.close();
+                    socket.close();
+                    System.out.println("client " + name + " disconnected");
                     break;
                 }
-                if (message.startsWith("/upload")) {
+                if (message.equals("/upload")) {
                     recieveFiles(message);
-                    break;
+
+                } else if (message.equals("/download")) {
+                    sendFiles(message);
                 }
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
