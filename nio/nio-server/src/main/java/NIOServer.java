44521f7b5f33a9159975d19a2df5b5427357bb26
RZN1505 <reznickova.zlata@yandex.ru>
2020-09-01 15:39:13 +0300

    lesson3

--- nio/nio-server/src/main/java/NIOServer.java
+++ nio/nio-server/src/main/java/NIOServer.java
@@ -1,108 +1,88 @@
+package com.avaldes.tutorial;
+
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
-import java.util.function.Consumer;
+import java.util.Set;
 
-public class NIOServer implements Runnable{
+public class NIOServer {
+    private static final int BUFFER_SIZE = 1024;
+    private static Selector selector = null;
 
-    // буффер для передачи сообщений в сеть и для чтения сообщений из сети
-    private final ByteBuffer buffer = ByteBuffer.allocate(256);
-
-    // считчик для ников
-    private static int cnt = 0;
-
-    @Override
-    public void run() {
+    public static void main(String[] args) {
+        logger("Starting server...");
         try {
-            // получаем инстанс объекта сервера
-            ServerSocketChannel server = ServerSocketChannel.open();
-            // запускаем сервер на порту 8189
-            server.bind(new InetSocketAddress(8189));
-            System.out.println("Server started on 8189");
-            // получаем инстанс селектора
-            Selector selector = Selector.open();
-            // до регистрации событий на селекторе делаем неблокирующий режим (порядок важен)
-            server.configureBlocking(false);
-            // регистрация селектора, то есть селектор будет слушать порт
-            // и принимать события типа соединение от клиентов
-            server.register(selector, SelectionKey.OP_ACCEPT);
+            InetAddress hostIP = InetAddress.getLocalHost();
+            int port = 8189;
 
-            while (server.isOpen()) {
-                selector.select(); // блокирующая операция, получаем коллекцию событий (SelectionKeys)
-                // формируем итератор
-                Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
-                while (keyIterator.hasNext()) {
-                    // обработка события сразу
-                    SelectionKey key = keyIterator.next();
+            logger(String.format("Trying to accept connections on %s:%d...",
+                    hostIP.getHostAddress(), port));
+            selector = Selector.open();
+            ServerSocketChannel mySocket = ServerSocketChannel.open();
+            ServerSocket serverSocket = mySocket.socket();
+            InetSocketAddress address = new InetSocketAddress(hostIP, port);
+            serverSocket.bind(address);
+
+            mySocket.configureBlocking(false);
+            int ops = mySocket.validOps();
+            mySocket.register(selector, ops, null);
+            while (true) {
+
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> i = selectedKeys.iterator();
+
+                while (i.hasNext()) {
+                    SelectionKey key = i.next();
+
                     if (key.isAcceptable()) {
-                        // отловили соединение
-                        cnt++;
-                        // канал получаем из сервера путем вызова метода accept (так же как в IO)
-                        SocketChannel channel = ((ServerSocketChannel) key.channel()).accept();
-                        // выключаем блокирующий режим
-                        channel.configureBlocking(false);
-                        // регистрируем селектор уже на события чтения, в аттач можно кинуть любой объект
-                        channel.register(selector, SelectionKey.OP_READ, "user#" + cnt);
-                        channel.register(selector, SelectionKey.OP_WRITE, "user#" + cnt);
-                        System.out.println("Accepted connection: " + "user#" + cnt);
+                        processAcceptEvent(mySocket, key);
+                    } else if (key.isReadable()) {
+                        processReadEvent(key);
                     }
-                    if (key.isReadable()) {
-                        // отловили пачку байт (чтение)
-                        System.out.println("Handled read operation");
-                        // достали канал
-                        SocketChannel channel = (SocketChannel) key.channel();
-                        // будем пихать в него байты
-                        StringBuilder message = new StringBuilder();
-                        int read = 1;
-                        while (true) {
-                            // читем пачку байт
-                            read = channel.read(buffer);
-                            // если 0 то только подключился
-                            if (read <= 0) {
-                                // если -1 то отключился, нужно закрыть канал (иначе будут сыпаться -1)
-                                if (read == -1) {
-                                    channel.close();
-                                    System.out.println("Client " + key.attachment() + " disconnected");
-                                }
-                                break;
-                            }
-                            // бефер в чтение
-                            buffer.flip();
-                            while (buffer.hasRemaining()) {
-                                // читаем данные из буфера
-                                message.append((char)buffer.get());
-                            }
-                            // буфер в запись
-                            buffer.rewind();
-                        }
-                        // сообщение которое отдаем клиенту
-                        String msg = key.attachment() + ": " + message.toString();
-                        // идем по всем ключам на селекторе
-                        for (SelectionKey selectionKey : selector.keys()) {
-                            // могут иметься закрытые и испорченные каналы
-                            // рассылаем только по валидным каналам с типом SocketChannel
-                            if (key.isValid() && selectionKey.channel() instanceof SocketChannel) {
-                                ((SocketChannel) selectionKey.channel())
-                                        .write(ByteBuffer.wrap(msg.getBytes()));
-                            }
-                        }
-                    }
-                    // обязательно чистим отобранные ключи, иначе будут скапливаться
-                    keyIterator.remove();
+                    i.remove();
                 }
             }
         } catch (IOException e) {
+            logger(e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        // запускаем в отдельном потоке так как имеется 1 блокирующая операция
-        new Thread(new NIOServer()).start();
+    private static void processAcceptEvent(ServerSocketChannel mySocket,
+                                           SelectionKey key) throws IOException {
+
+        logger("Connection Accepted...");
+        SocketChannel myClient = mySocket.accept();
+        myClient.configureBlocking(false);
+
+        myClient.register(selector, SelectionKey.OP_READ);
+    }
+
+    private static void processReadEvent(SelectionKey key)
+            throws IOException {
+        logger("Inside processReadEvent...");
+        SocketChannel myClient = (SocketChannel) key.channel();
+        ByteBuffer myBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+        myClient.read(myBuffer);
+        String data = new String(myBuffer.array()).trim();
+        if (data.length() > 0) {
+            logger(String.format("Message Received.....: %s\n", data));
+            if (data.equalsIgnoreCase("*exit*")) {
+                myClient.close();
+                logger("Closing Server Connection...");
+            }
+        }
+    }
+
+    public static void logger(String msg) {
+        System.out.println(msg);
     }
 }
